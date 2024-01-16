2837ef7f2b2b4c11fc40338e89877ac2a2e903b6
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-01-16 15:35:36 -0300

    Problem 3 solved

---
+++ problem_3.py
@@ -0,0 +1,14 @@
+LPF = 600851475143
+
+def largest_prime_factor(n):
+    factor = 2
+    while factor * factor <= n:
+        if n % factor:
+            factor += 1
+        else:
+            n //= factor
+    return n
+
+if __name__ == "__main__":
+    result = largest_prime_factor(LPF)
+    print(result)
