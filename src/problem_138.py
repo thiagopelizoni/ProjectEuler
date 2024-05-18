53be98ad51437a4ca38bd19657ef2c54b8f6421d
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-05-18 08:50:50 -0300

    Problem 138 solved

---
+++ src/problem_138.py
@@ -0,0 +1,33 @@
+# Problem: https://projecteuler.net/problem=138
+from math import isqrt
+
+def main():
+    target = 12
+    count = 0
+    answer = 0
+
+    n = 1
+    while count < target:
+        MC_candidates = []
+
+        if (sqrt := isqrt(5 * n * n + 1)) ** 2 == 5 * n * n + 1:
+            MC_candidates.append(2 * n + sqrt)
+        if (sqrt := isqrt(5 * n * n - 1)) ** 2 == 5 * n * n - 1:
+            MC_candidates.append(2 * n + sqrt)
+        if (sqrt := isqrt(5 * n * n + 2)) ** 2 == 5 * n * n + 2:
+            MC_candidates.append(n + sqrt)
+        if (sqrt := isqrt(5 * n * n - 2)) ** 2 == 5 * n * n - 2:
+            MC_candidates.append(n + sqrt)
+
+        for m in MC_candidates:
+            a, b, c = m * m - n * n, 2 * m * n, m * m + n * n
+            count += 1
+            answer += c
+            print(a, b, c)
+
+        n += 1
+
+    print(f"Answer: {answer}")
+
+if __name__ == "__main__":
+    main()
