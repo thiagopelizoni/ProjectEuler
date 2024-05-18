afb791ebba4a8263e966ef688cef5767445d2a83
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-05-17 21:14:55 -0300

    Problem 60 solved

--- src/problem_060.py
+++ src/problem_060.py
@@ -1,21 +1,55 @@
 # Problem: https://projecteuler.net/problem=60
-# Failed attempt to silve this problem
-from sympy import primerange
-from itertools import permutations
+import sympy
+import functools
+import math
 
-def is_prime_concat(set_primes, a, b):
-    return (int(str(a) + str(b)) in set_primes) and (int(str(b) + str(a)) in set_primes)
+def main():
+    PRIME_LIMIT = 100000  # Arbitrary initial cutoff
+    primes = list(sympy.primerange(2, PRIME_LIMIT))
+    
+    def find_set_sum(prefix, targetsize, sumlimit):
+        if len(prefix) == targetsize:
+            return sum(primes[i] for i in prefix)
+        else:
+            istart = 0 if (len(prefix) == 0) else (prefix[-1] + 1)
+            for i in range(istart, len(primes)):
+                if primes[i] > sumlimit:
+                    break
+                if all((is_concat_prime(i, j) and is_concat_prime(j, i)) for j in prefix):
+                    prefix.append(i)
+                    result = find_set_sum(prefix, targetsize, sumlimit - primes[i])
+                    prefix.pop()
+                    if result is not None:
+                        return result
+            return None
 
-def find_prime_set():
-    primes = list(primerange(3, 10000))
-    set_primes = set(primes)
+    @functools.lru_cache(maxsize=None)
+    def is_concat_prime(x, y):
+        return is_prime(int(str(primes[x]) + str(primes[y])))
 
-    for a in primes:
-        for b in [x for x in primes if x > a and is_prime_concat(set_primes, a, x)]:
-            for c in [x for x in primes if x > b and is_prime_concat(set_primes, a, x) and is_prime_concat(set_primes, b, x)]:
-                for d in [x for x in primes if x > c and is_prime_concat(set_primes, a, x) and is_prime_concat(set_primes, b, x) and is_prime_concat(set_primes, c, x)]:
-                    for e in [x for x in primes if x > d and is_prime_concat(set_primes, a, x) and is_prime_concat(set_primes, b, x) and is_prime_concat(set_primes, c, x) and is_prime_concat(set_primes, d, x)]:
-                        return a + b + c + d + e
+    def is_prime(x):
+        if x < 0:
+            raise ValueError()
+        elif x in (0, 1):
+            return False
+        else:
+            end = math.isqrt(x)
+            for p in primes:
+                if p > end:
+                    break
+                if x % p == 0:
+                    return False
+            for i in range(primes[-1] + 2, end + 1, 2):
+                if x % i == 0:
+                    return False
+            return True
 
-answer = find_prime_set()
-print(answer)
+    sumlimit = PRIME_LIMIT
+    while True:
+        setsum = find_set_sum([], 5, sumlimit - 1)
+        if setsum is None:
+            return str(sumlimit)
+        sumlimit = setsum
+
+if __name__ == "__main__":
+    print(main())
