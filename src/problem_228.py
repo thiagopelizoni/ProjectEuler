b2d8dcbc46ad821691ad32351a7047f4b366ba7c
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2025-08-22 08:56:35 -0300

    Refactoring 228 problem after solve it

--- src/problem_228.py
+++ src/problem_228.py
@@ -4,22 +4,29 @@ from multiprocessing import Pool
 from fractions import Fraction
 from tqdm import tqdm
 
-def create_set_for_n(n):
-    local_set = set()
-    for k in range(n):
-        fraction = Fraction(k, n)
-        local_set.add(fraction)
-    return local_set
 
-if __name__ == '__main__':
-    starting_n = 1864
-    ending_n = 1909
-    all_n_values = list(range(starting_n, ending_n + 1))
-    number_of_processes = multiprocessing.cpu_count()
-    with Pool(number_of_processes) as pool:
-        sets_for_each_n = list(tqdm(pool.imap(create_set_for_n, all_n_values), total=len(all_n_values)))
-    total_unique_fractions = set()
-    for current_set in sets_for_each_n:
-        total_unique_fractions.update(current_set)
-    number_of_sides = len(total_unique_fractions)
-    print(number_of_sides)
+def create_set_for_n(n):
+    fraction_set = set()
+    for numerator in range(n):
+        value = Fraction(numerator, n)
+        fraction_set.add(value)
+    return fraction_set
+
+
+if __name__ == "__main__":
+    start_n = 1864
+    end_n = 1909
+    n_values = list(range(start_n, end_n + 1))
+    cpu_count = multiprocessing.cpu_count()
+
+    with Pool(cpu_count) as pool:
+        sets_per_n = list(
+            tqdm(pool.imap(create_set_for_n, n_values), total=len(n_values))
+        )
+
+    unique_fractions = set()
+    for fraction_set in sets_per_n:
+        unique_fractions.update(fraction_set)
+
+    total_sides = len(unique_fractions)
+    print(total_sides)
