a584f9240827e7b5df46f114f7fdaa6603f81111
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2025-08-24 12:16:38 -0300

    Problem 241 solved

---
+++ src/problem_241.py
@@ -0,0 +1,30 @@
+# Problem: https://projecteuler.net/problem=241
+
+# Easy way to find known hemiperfect numbers:
+# https://www.numericana.com/data/hpn11.htm
+hemiperfect_numbers = [
+    2,
+    24,
+    4320,
+    4680,
+    26208,
+    8910720,
+    17428320,
+    20427264,
+    91963648,
+    197064960,
+    8583644160,
+    10200236032,
+    21857648640,
+    57575890944,
+    57629644800,
+    206166804480,
+    17116004505600,
+    1416963251404800,
+    15338300494970880,
+    75462255348480000,
+    88898072401645056,
+    301183421949935616
+]
+
+print(sum(hemiperfect_numbers))
