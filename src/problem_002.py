5fb90d32b6fc90c4bc70038ce6202b9a807e9f74
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2025-08-30 14:32:35 -0300

    Refatoring Problem 2

--- src/problem_002.py
+++ src/problem_002.py
@@ -1,16 +1,20 @@
 # Problem: https://projecteuler.net/problem=2
+from tqdm import tqdm
 
 LIMIT = 4000000
 
-def sum_fibonacci_pairs(limit):
-    result = 0
-    a, b = 1, 2
-    while a <= limit:
-        if a % 2 == 0:
-            result += a
-        a, b = b, a + b
-    return result
+def sum_even_fibonacci(limit: int) -> int:
+    total_sum = 0
+    a, b = 2, 8
+    
+    with tqdm() as pbar:
+        while a <= limit:
+            total_sum += a
+            a, b = b, 4 * b + a
+            pbar.update(1)
+        
+    return total_sum
 
 if __name__ == "__main__":
-    answer = sum_fibonacci_pairs(LIMIT)
+    answer = sum_even_fibonacci(LIMIT)
     print(answer)
